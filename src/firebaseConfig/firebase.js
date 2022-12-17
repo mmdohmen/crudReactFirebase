6708eb3e89fb04b5ba29020386905e16b5a073bd
Zeta <83716941+mmdohmen@users.noreply.github.com>
2022-12-17 13:21:33 -0300

    Update firebase.js

--- src/firebaseConfig/firebase.js
+++ src/firebaseConfig/firebase.js
@@ -6,12 +6,7 @@ import { getFirestore } from "@firebase/firestore"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB6EGw5VEcoU3hFruvTjoUGmjTy0w-HJ64",
-  authDomain: "crud-22808-mdohmen.firebaseapp.com",
-  projectId: "crud-22808-mdohmen",
-  storageBucket: "crud-22808-mdohmen.appspot.com",
-  messagingSenderId: "961140162015",
-  appId: "1:961140162015:web:a948e50a0ae2a0f8ac986a"
+  
 };
 
 // Initialize Firebase
